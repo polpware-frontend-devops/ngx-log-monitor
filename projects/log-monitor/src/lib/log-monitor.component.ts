d8913340f5a48b03c67e92838f2d70942e995682
Tom Tang <tom@fake.com>
2021-11-20 14:34:23 -0500

    Upgrade to Angular 9

--- projects/log-monitor/src/lib/log-monitor.component.ts
+++ projects/log-monitor/src/lib/log-monitor.component.ts
@@ -1,57 +1,73 @@
 import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  Input, NgZone,
-  OnChanges,
-  SimpleChanges,
-  ViewChild
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    Input, NgZone,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    SimpleChanges,
+    ViewChild
 } from '@angular/core';
-import {LogMessage} from './models/log-message.model';
-import {normalizeLogMessage} from './helpers/log-message.helper';
+import { LogMessage } from './models/log-message.model';
+import { normalizeLogMessage } from './helpers/log-message.helper';
+import { Observable, Subscription } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
-  selector: 'log-monitor',
-  templateUrl: './log-monitor.component.html',
-  styleUrls: ['./log-monitor.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'log-monitor',
+    templateUrl: './log-monitor.component.html',
+    styleUrls: ['./log-monitor.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LogMonitorComponent implements OnChanges, AfterViewInit {
+export class LogMonitorComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit {
 
-  @Input() title;
-  @Input() logStream: LogMessage;
-  @Input() history: LogMessage[] = [];
-  @Input() theme: 'dark' | 'light' = 'dark';
-  @Input() icons = true;
-  @Input() customClass = 'log-container';
-  @Input() animated = true;
-  @ViewChild('container', {static: false}) container: ElementRef;
+    @Input() delay = 500;
+    @Input() title;
+    @Input() logStream: Observable<LogMessage>;
+    @Input() history: LogMessage[] = [];
+    @Input() theme: 'dark' | 'light' = 'dark';
+    @Input() icons = true;
+    @Input() customClass = 'log-container';
+    @Input() animated = true;
+    @ViewChild('container') container: ElementRef;
 
-  constructor(private zone: NgZone) { }
+    _history: LogMessage[] = [];
 
-  ngOnChanges(changes: SimpleChanges) {
+    private _subr: Subscription;
 
-    if (changes['history']) {
-      this.history = changes['history'].currentValue.map(normalizeLogMessage);
+    constructor(private zone: NgZone) { }
+
+    ngOnInit() {
+        if (this.logStream) {
+            this._subr = this.logStream
+                .pipe(delay(this.delay))
+                .subscribe(a => {
+                    this._history.push(normalizeLogMessage(a));
+                    this.zone.run(() => {
+                        setTimeout(() => this.scrollToBottom());
+                    });
+                });
+        }
     }
 
-    if (changes['logStream'] && changes['logStream'].currentValue) {
-
-      this.zone.run(() => {
-        const normalizedMsg = normalizeLogMessage(changes['logStream'].currentValue);
-        this.history.push(normalizedMsg);
-        setTimeout(() => this.scrollToBottom());
-      });
+    ngOnDestroy() {
+        this._subr && this._subr.unsubscribe();
     }
-  }
 
-  ngAfterViewInit() {
-    this.scrollToBottom();
-  }
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['history']) {
+            this._history = changes['history'].currentValue.map(normalizeLogMessage);
+        }
+    }
 
-  private scrollToBottom() {
-    this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
-  }
+    ngAfterViewInit() {
+        this.scrollToBottom();
+    }
+
+    private scrollToBottom() {
+        this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
+    }
 
 }
