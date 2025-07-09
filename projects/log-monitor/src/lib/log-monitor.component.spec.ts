a8a7276e90e52c22598583b431030855b2768e6d
Tomcat <polpware@gmail.com>
2025-07-09 06:52:41 -0400

    Update for Angular 11

--- projects/log-monitor/src/lib/log-monitor.component.spec.ts
+++ projects/log-monitor/src/lib/log-monitor.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LogMonitorComponent } from './log-monitor.component';
 
@@ -6,7 +6,7 @@ describe('LogMonitorComponent', () => {
   let component: LogMonitorComponent;
   let fixture: ComponentFixture<LogMonitorComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LogMonitorComponent ]
     })
