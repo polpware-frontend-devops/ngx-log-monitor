9e50c5c467d9b79b8205b299781e4a73a04ce198
Tomcat <polpware@gmail.com>
2025-07-17 22:09:49 -0400

    Update to Angular 19

--- projects/log-monitor/src/lib/log-monitor.component.ts
+++ projects/log-monitor/src/lib/log-monitor.component.ts
@@ -20,7 +20,8 @@ import { delay } from 'rxjs/operators';
     selector: 'log-monitor',
     templateUrl: './log-monitor.component.html',
     styleUrls: ['./log-monitor.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: false
 })
 export class LogMonitorComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit {
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import {timer} from 'rxjs';
 import {map, take, tap} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
 
